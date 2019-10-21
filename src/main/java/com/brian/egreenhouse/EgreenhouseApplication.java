ed0d49802ee29a75840992feb4c19e9db68a6336
brian <kimutaibryan@gmail.com>
2019-10-21 10:28:28 -0700

    initial commit

---
+++ src/main/java/com/brian/egreenhouse/EgreenhouseApplication.java
@@ -0,0 +1,13 @@
+package com.brian.egreenhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgreenhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgreenhouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brian/egreenhouse/EgreenhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.egreenhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgreenhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
